2c3bfc88fcbe79968bd1f2df2d6ee15fc88e9234
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-25 01:59:08 -0400

    Minimize MyGcmListener

--- app/src/main/java/com/example/simplegcm/MyGcmListenerService.java
+++ app/src/main/java/com/example/simplegcm/MyGcmListenerService.java
@@ -26,35 +26,6 @@ public class MyGcmListenerService extends GcmListenerService {
 
         Log.d(TAG, "From: " + from);
         Log.d(TAG, "Message: " + message);
-
-        if (from.startsWith("/topics/")) {
-
-        } else {
-
-        }
-
-        sendNotification(message);
-    }
-
-    private void sendNotification(String message) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0, notificationBuilder.build());
     }
 }
 
